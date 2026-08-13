e7a9e4c402569c2926bd8720484e781663da10e6
Mostafa Hamza <mostafa.mn.abbas@gmail.com>
2026-08-13 17:03:17 -0400

    Remove nested CLEAN folder from tracking

--- CLEAN/IndepAssoc-main/tests/testthat/test-description.R
+++
@@ -1,9 +0,0 @@
-test_that("DESCRIPTION declares utils in Imports", {
-  desc_path <- testthat::test_path("../../DESCRIPTION")
-  if (!file.exists(desc_path)) {
-    desc_path <- system.file("DESCRIPTION", package = "IndepAssoc")
-  }
-  desc <- read.dcf(desc_path)
-  imports <- trimws(unlist(strsplit(desc[1, "Imports"], "[,\n]")))
-  expect_true("utils" %in% imports)
-})
